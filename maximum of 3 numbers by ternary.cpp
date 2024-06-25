0d29d722bb3b27f950e42ead4f26e6f1daa27673
kish0rekumar <128585816+kish0rekumar@users.noreply.github.com>
2024-06-25 09:49:27 +0530

    Add files via upload

---
+++ maximum of 3 numbers by ternary.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class ternary{
+int a,b;
+public:
+};
+void main(a,b){
+	char d=(a>b)?a:b;
+	cout<<d;
+}
+int main(){
+	int a=1, b=2;
+	ternary a;
+	ternary b;
+	p1.ternary(a,b);
+}
+
+
